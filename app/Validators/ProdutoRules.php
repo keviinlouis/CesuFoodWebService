2a462463828c539abf2a4b81d4233c30d4bf5362
Kevin  Louis <keviinlouiis@gmail.com>
2018-05-28 01:41:28 -0300

    update produto rules

--- app/Validators/ProdutoRules.php
+++ app/Validators/ProdutoRules.php
@@ -13,6 +13,7 @@
 namespace App\Validators;
 
 use App\Entities\Produto;
+use App\Rules\RuleFileExistsOnTmp;
 use Illuminate\Validation\Rule;
 
 
@@ -30,7 +31,13 @@ class ProdutoRules
     static public function store(): array
     {
         return [
-            //TODO Implementar store
+            'nome' => 'required|string|min:3',
+            'valor' => 'required|numeric|not_in:0',
+            'descricao' => 'required|string|min:10',
+            'categoria_id' => 'required|exists:categorias,id',
+            'status' => 'required|between:0,1',
+            'fotos' => 'required|array|min:1',
+            'fotos.*' => ['required', new RuleFileExistsOnTmp()],
         ];
     }
 
@@ -41,7 +48,14 @@ class ProdutoRules
     static public function update(): array
     {
         return [
-            //TODO Implementar update
+            'nome' => 'string|min:3',
+            'valor' => 'numeric|not_in:0',
+            'descricao' => 'string|min:10',
+            'categoria_id' => 'exists:categorias,id',
+            'status' => 'between:0,1',
+            'fotos_removidas' => 'array',
+            'fotos_adicionadas' => 'array',
+            'fotos_adicionadas.*' => ['required', new RuleFileExistsOnTmp()],
         ];
     }
 }
